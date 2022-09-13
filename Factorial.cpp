9039e4f4f926453208950a3b63936c54c5a00eda
rANBIR772 <98515691+rANBIR772@users.noreply.github.com>
2022-09-13 10:39:35 +0530

    Add files via upload

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,13 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+class factorial
+{
+    public:
+    int i,fact=1,num;
+    void input();
+    void output()
+    {
+        for(i=1;i<=num;i++)
+        fact=i*fact;
+        cout<<"Factorial of "<<num<<" is: "<<fact;
+    }
+};
+void factorial::input()
+    {
+        cout<<"Enter a number:";
+        cin>>num;
+    }
 int main()
 {
-    int i, fact=1, number;
-    cout<<"Enter a number: ";
-    cin>>number;
-    for(i=1;i<=number;i++){
-        fact=fact*i;
-    }
-    cout<<"Factorial of "<<number<<" is: "<<fact<<endl;
+    factorial obj;
+    obj.input();
+    obj.output();
     return 0;
 }
